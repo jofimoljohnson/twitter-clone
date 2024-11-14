9ad07efb3ca23694bf7cbc2e4b00e423801b8edf
jofimoljohnson <jofimolj@gmail.com>
2024-11-14 10:27:42 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import {v2 as cloudinary} from 'cloudinary'
@@ -17,6 +18,9 @@ cloudinary.config({
 })
 const app=express()
 const PORT=process.env.PORT || 5000
+const __dirname=path.resolve()
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
@@ -28,6 +32,15 @@ app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
 app.use("/api/notifications",notificationRoutes)
+
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
 
 
 
